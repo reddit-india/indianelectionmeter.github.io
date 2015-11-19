e7c018ba5488ff131daab1854b13a23cc848a249
Divjot Singh <bogas04@gmail.com>
2015-11-19 22:59:34 +0530

    add node_modules to gitignore

--- meteor/Knob.jsx
+++ meteor/Knob.jsx
@@ -5,11 +5,11 @@ Knob = React.createClass({
 	render: function () {
 		return (
 			React.createElement('div', { className: 'text-center' }
-			,React.createElement('input', { 
-		className: 'knob dial', 
-		type: 'text', 
-		'data-readonly': 'true', 
-		'data-min': 0, 
+			,React.createElement('input', {
+		className: 'knob dial',
+		type: 'text',
+		'data-readonly': 'true',
+		'data-min': 0,
 		'data-max': this.props.max,
 		'data-fgcolor': this.props.value > this.props.max/2 ? 'green' : this.props.value > this.props.max/3 ? 'orange' : 'red',
 		value: this.props.value
