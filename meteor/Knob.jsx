e8ac632b82f96dbe10ffb0789ab1847bde229da2
Divjot Singh <bogas04@gmail.com>
2015-11-19 23:00:18 +0530

    convert Knob.jsx to jsx syntax

--- meteor/Knob.jsx
+++ meteor/Knob.jsx
@@ -1,19 +1,23 @@
 Knob = React.createClass({
-	componentDidMount: function() {
-	    $('.knob').knob();
-	},
-	render: function () {
-		return (
-			React.createElement('div', { className: 'text-center' }
-			,React.createElement('input', {
-		className: 'knob dial',
-		type: 'text',
-		'data-readonly': 'true',
-		'data-min': 0,
-		'data-max': this.props.max,
-		'data-fgcolor': this.props.value > this.props.max/2 ? 'green' : this.props.value > this.props.max/3 ? 'orange' : 'red',
-		value: this.props.value
-			}))
-		);
-	}
+  componentDidMount: function() {
+    $('.knob').knob();
+  },
+  getKnobColor: function (value) {
+    return value > this.props.max/2 ? 'green' : value > this.props.max/3 ? 'orange' : 'red';
+  },
+  render: function () {
+    return (
+      <div className = "text-center">
+        <input
+          className = "knob dial"
+          type = "text"
+          data-readonly = "true"
+          data-min = {0}
+          data-max = {this.props.max}
+          data-fgcolor = {this.getKnobColor(this.props.value)}
+          value = {this.props.value}
+          />
+      </div>
+    );
+  }
 });
